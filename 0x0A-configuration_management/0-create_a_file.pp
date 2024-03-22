da9303828029aa189295c8afcc9342c449535724
Thugg55 <thuggeru7@gmail.com>
2024-03-22 07:01:07 -0700

    manifest creates a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# manifest to create a file owned by www-data with permission 744
+
+file { '/tmp/school':
+ensure => 'file',
+mode => '0744',
+owner => 'www-data',
+group => 'www-data',
+content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+learning extensively about Puppet
