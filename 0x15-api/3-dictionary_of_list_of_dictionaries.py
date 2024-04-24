d8e1fb7907dcbc28f201c9d593dc9a7fbd532f11
Thugg55 <tuggeru7@gmail.com>
2024-04-23 23:17:32 -0700

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Script export data in JSON format """
+
+import json
+import requests
+
+
+if __name__ == '__main__':
+    base_url = f"https://jsonplaceholder.typicode.com/users/"
+    users = requests.get(base_url).json()
+    with open("todo_all_employees.json", "w") as f:
+        json.dump({
+            user['id']: [{
+                "task": task['title'], "completed": task['completed'],
+                "username": user['username']
+                } for task in requests.get(f"{base_url}{user['id']}/todos").json()]
+            for user in users}, f)
