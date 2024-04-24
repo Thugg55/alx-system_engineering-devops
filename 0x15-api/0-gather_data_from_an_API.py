4f39ba1e1ded63d6456207f9336dc581fd013b9f
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2024-04-24 08:55:44 +0100

    Update 0-gather_data_from_an_API.py
    trying to get it right

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,12 @@
 """
 import requests
 import sys
+from sys import argv, exit
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_idg"
-g   todos_url = f"{base_url}/todos?userId={employee_id}"
+    user_url = f"{base_url}/users/{employee_id)"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
     # Fetching user information
     user_response = requests.get(user_url)
