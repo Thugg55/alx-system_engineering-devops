84c5aab38cc43ab3fca6d38beab40bd63fc9f8d8
Thugg55 <tuggeru7@gmail.com>
2024-04-24 20:52:50 -0700

    corrected

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     users = requests.get(base_url).json()
     with open("todo_all_employees.json", "w") as f:
         json.dump({
-            user['id']: [{
+           user['id']: [{
                 "task": task['title'], "completed": task['completed'],
                 "username": user['username']
-                } for task in requests.get(f"{base_url}{user['id']}/todos").json()]
-            for user in users}, f)
+                } for task in requests.get(f"{base_url}{user['id']}/todos")
+                .json()]
+           for user in users}, f)
