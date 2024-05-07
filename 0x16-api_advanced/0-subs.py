5abbfbbe767427e897c2db025a11d0eec2eca790
Thugg55 <tuggeru7@gmail.com>
2024-05-07 12:59:59 -0700

    corrected

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,21 @@ number of subcribers
 """
 import requests
 
-def number_of_subscribers(subreddit):
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        headers = {'User-Agent': 'Custom User Agent'}
-        # Set a custom User-Agent to avoid Too Many Requests error
-        response = requests.get(url, headers=headers)
-                    
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-         else:
-            return 0
 
+def number_of_subscribers(subreddit):
+    """
+    Args:
+    subreddit: name
+    Returns: numbef id subcribers to the subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    # Set a custom User-Agent to avoid Too Many Requests error
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
