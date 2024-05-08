7958b85b945a4a9b16f1f67d7ae71d24065e5ab5
Thugg55 <tuggeru7@gmail.com>
2024-05-08 16:01:41 -0700

    puppet fix

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#puppet script fix bad phpp extensions in wordpress file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
